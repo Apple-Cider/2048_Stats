f20820cf82d075934f2b89879348db450ec758d6
Apple-Cider <mbank@kent.edu>
2015-02-05 06:46:27 -0500

    added mbstock margin convention and shrunk hello-img

--- assets/js/viz.js
+++ assets/js/viz.js
@@ -1,8 +1,23 @@
+var margin = {top: 20, right: 10, bottom: 20, left: 10};
+var width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
+
+var x = d3.time.scale().range([0, width]),
+	y = d3.scale.linear().range([height, 0]);
+	
+var maxScore = 0;
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+
 function parseLog(logPath) {
 	d3.csv(logPath, function(d) {
 		var formatter = d3.time.format("%-m/%-d/%Y %-I:%-M:%-S %p");
-		var notes = d.notes;
-		console.log("d.notes = " + notes);
+		if(+d.score > maxScore) maxScore = +d.score;
 		return {
 			score: +d.score,
 			largest: +d.largest,
@@ -16,6 +31,11 @@ function saySomething(error, rows) {
 	for(var i = 0; i < rows.length; i++){
 		console.log(rows[i].score);
 	}
+	visualize(rows);
+}
+
+function visualize(data) {
+	console.log("maxScore = " + maxScore);
 }
 
 window.onload = function() {
--- server.py
+++ server.py
@@ -5,15 +5,10 @@ class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		print("rendering page...")
 		self.render("viz.html")
-		
-class AssetHandler(tornado.web.RequestHandler):
-	def get(self, stuff):
-		print("accessing an asset... " + stuff)
 
-		
+#TODO: extract absolute path and reference from a non-repo config file
 application = tornado.web.Application([
 	(r"/", MainHandler),
-	#(r"/assets/(.*)", AssetHandler),
 	(r"/assets/(.*)", tornado.web.StaticFileHandler, {"path": "C:/Users/enduser/Desktop/Documents/Programming/Javascript/2048_Stats/assets/"}),
 ], debug=True)
 
