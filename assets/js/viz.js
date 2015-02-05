90a16973ebd920bfbcc283e2661bf4fa780daa88
Apple-Cider <mbank@kent.edu>
2015-02-05 05:57:55 -0500

    d3js says hello to the project by parsing the log

--- assets/js/viz.js
+++ assets/js/viz.js
@@ -1,13 +1,25 @@
 function parseLog(logPath) {
 	d3.csv(logPath, function(d) {
-		var formatter = d3.time.format("%-m/%-d/%Y %-I:%-M:%-S %p");
+		//var formatter = d3.time.format("%-m/%-d/%Y %-I:%-M:%-S %p");
+		var notes = d.notes;
 		return {
 			score: +d.score,
-			largest: +d.largest,
-			timestamp: formatter(d.date + " " + d.time),
-			notes: d.notes.split(";")
+			largest: +d.largest
+			//timestamp: formatter(d.date + " " + d.time),
+			//notes: d.notes.split(";")
 		};
-	});
+	}, saySomething);
 }
 
-parseLog("assets/data/log.csv");
+function saySomething(error, rows) {
+	for(var i = 0; i < rows.length; i++){
+		console.log(rows[i].score);
+	}
+}
+
+//window.onload = parseLog("assets/data/log.csv");
+window.onload = function() {
+	//alert("we're done here...");
+	//alert("we'll get somewhere soon...");
+	parseLog("assets/data/log.csv");
+};
--- server.py
+++ server.py
@@ -3,15 +3,18 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
+		print("rendering page...")
 		self.render("viz.html")
 		
 class AssetHandler(tornado.web.RequestHandler):
-	def get(self):
-		pass
+	def get(self, stuff):
+		print("accessing an asset... " + stuff)
 
+		
 application = tornado.web.Application([
 	(r"/", MainHandler),
-	(r"/assets/", AssetHandler),
+	#(r"/assets/(.*)", AssetHandler),
+	(r"/assets/(.*)", tornado.web.StaticFileHandler, {"path": "C:/Users/enduser/Desktop/Documents/Programming/Javascript/2048_Stats/assets/"}),
 ])
 
 if __name__ == "__main__":
